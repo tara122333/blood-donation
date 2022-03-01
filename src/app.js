38b8b60195d852998a800a32c1c818970cfda7b1
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-03-01 12:09:37 +0530

    heroku server add

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 80;
+const port = process.env.PORT || 3000
 
 const staticPath = path.join(__dirname,"../public");
 
